153e3ebc9d94f6f71d90fb160e63be34b6f70624
Timur Moziev <tr@cinnabar.ru>
2023-12-30 00:13:24 +0300

    init

---
+++ migrate.js
@@ -0,0 +1,186 @@
+let dialect;
+
+const migrations = [];
+
+const tables = {};
+
+function resetContext() {
+  migrations.length = 0;
+  const props = Object.getOwnPropertyNames(tables);
+  for (let i = 0; i < props.length; i++) {
+    delete tables[props[i]];
+  }
+}
+
+function getSqlDialect() {
+  return dialect;
+}
+
+function setSqlDialect(value) {
+  dialect = value;
+}
+
+function createMigration() {
+  migrations.push({
+    queries: [
+      `INSERT INTO migrations (revision, app_version, date_migrated) VALUES (?, ?, ?);`,
+    ],
+    args: [
+      migrations.length,
+      process.env.npm_package_version,
+      Math.round(Date.now() / 1000),
+    ],
+  });
+}
+
+function addSql(query, args) {
+  const currentMigration = migrations[migrations.length - 1];
+  currentMigration.queries.push(query);
+  if (args) {
+    currentMigration.args.push(...args);
+  }
+}
+
+function getColumnQueryPart(column) {
+  const columnQuery = [];
+  columnQuery.push(`"${column.name}"`);
+  columnQuery.push(column.type.toUpperCase());
+  if (column.primaryKey) {
+    columnQuery.push("PRIMARY KEY");
+  }
+  if (column.autoIncrement) {
+    columnQuery.push("AUTOINCREMENT");
+  }
+  if (column.notNull) {
+    columnQuery.push("NOT NULL");
+  }
+
+  return columnQuery.join(" ");
+}
+
+function getTableCreationSqlQuery(name, columns) {
+  const columnsQuery = [];
+
+  const primaryKeys = [];
+  const uniques = [];
+
+  for (const [columnName, column] of Object.entries(columns)) {
+    if (!column.id && column.primaryKey) {
+      primaryKeys.push(`"${columnName}"`);
+    }
+
+    if (column.unique) {
+      uniques.push(`"${columnName}"`);
+    }
+
+    columnsQuery.push(getColumnQueryPart(column));
+  }
+
+  if (primaryKeys.length > 0) {
+    columnsQuery.push(`PRIMARY KEY(${primaryKeys.join(", ")})`);
+  }
+
+  if (uniques.length > 0) {
+    columnsQuery.push(`UNIQUE(${uniques.join(", ")})`);
+  }
+
+  return `CREATE TABLE "${name}" (${columnsQuery.join(", ")});`;
+}
+
+function createTable(name, columns) {
+  const currentMigration = migrations[migrations.length - 1];
+
+  tables[name] = {};
+
+  for (const [columnName, column] of Object.entries(columns)) {
+    if (column.id) {
+      column.type = "integer";
+      column.primaryKey = true;
+      column.autoIncrement = true;
+    }
+
+    tables[name][columnName] = { type: column.type };
+  }
+
+  currentMigration.queries.push(getTableCreationSqlQuery(name, columns));
+}
+
+function addTableColumn(tableName, column) {
+  const currentMigration = migrations[migrations.length - 1];
+  const currentColumn = tables[tableName][column.name];
+
+  let query = `ALTER TABLE "${tableName}" ADD COLUMN ${getColumnQueryPart(
+    column
+  )};`;
+  currentMigration.queries.push(query);
+}
+
+function changeTableColumn(tableName, column) {
+  const currentMigration = migrations[migrations.length - 1];
+  const currentColumn = tables[tableName][column.name];
+
+  currentMigration.queries.push(
+    getTableCreationSqlQuery(tableName, tables[tableName])
+  );
+}
+
+function getMigrationRevisionSqlQuery() {
+  return "CREATE TABLE IF NOT EXISTS migrations (revision INTEGER NOT NULL PRIMARY KEY, app_version TEXT NOT NULL, date_migrated INTEGER NOT NULL); SELECT MAX(revision) as latest_revision, app_version, date_migrated FROM migrations;";
+}
+
+function getMigrationsSqlBundle(latestMigration) {
+  const queries = [];
+  const args = [];
+
+  queries.push("BEGIN TRANSACTION;");
+
+  if (latestMigration.latest_revision != null) {
+    console.log(
+      `Last database migration: ${new Date(
+        latestMigration.date_migrated * 1000
+      ).toISOString()} (r${latestMigration.latest_revision}, v${
+        latestMigration.app_version
+      })`
+    );
+  } else {
+    latestMigration.latest_revision = 0;
+    console.log(`New database`);
+  }
+
+  if (latestMigration.latest_revision < migrations.length) {
+    console.log(`Migrating to revision ${migrations.length - 1}...`);
+    for (
+      let revision = latestMigration.latest_revision;
+      revision < migrations.length;
+      revision++
+    ) {
+      if (revision > 0) {
+        console.log(`...migration from r${revision - 1} to r${revision}...`);
+      }
+
+      queries.push(...migrations[revision].queries);
+      args.push(...migrations[revision].args);
+    }
+    console.log("...done migrating");
+  } else {
+    console.log(`Database is up-to-date`);
+  }
+
+  queries.push("COMMIT TRANSACTION;");
+  queries.push("VACUUM;");
+
+  return { query: queries.join("\n"), args };
+}
+
+export default {
+  resetContext,
+  getSqlDialect,
+  setSqlDialect,
+  createMigration,
+  addSql,
+  createTable,
+  addTableColumn,
+  changeTableColumn,
+  getMigrationRevisionSqlQuery,
+  getMigrationsSqlBundle,
+};
---
+++ migrate.test.js
@@ -0,0 +1,93 @@
+import cfMigrations from "./migrate.js";
+import assert from "assert";
+
+function createBaseMigration() {
+  cfMigrations.resetContext();
+  cfMigrations.createMigration();
+  cfMigrations.createTable("species", [
+    { name: "id", id: true },
+    { name: "name", type: "text", notNull: true },
+    { name: "origin", type: "text" },
+    { name: "population", type: "integer" },
+  ]);
+}
+
+describe("Empty run", function () {
+  it("should return correct SQL query", function () {
+    assert.equal(
+      cfMigrations.getMigrationRevisionSqlQuery(),
+      "CREATE TABLE IF NOT EXISTS migrations (revision INTEGER NOT NULL PRIMARY KEY, app_version TEXT NOT NULL, date_migrated INTEGER NOT NULL); SELECT MAX(revision) as latest_revision, app_version, date_migrated FROM migrations;"
+    );
+  });
+  it("should return correct SQL query", function () {
+    const query = cfMigrations.getMigrationsSqlBundle({});
+    assert.equal(
+      query.query,
+      `BEGIN TRANSACTION;
+COMMIT TRANSACTION;
+VACUUM;`
+    );
+    assert.equal(query.args.length, 0);
+  });
+});
+
+describe("Adding SQL", function () {
+  it("should add a SQL query", function () {
+    cfMigrations.createMigration();
+    cfMigrations.addSql("VACUUM;", ["testPurposes"]);
+  });
+  it("should return correct SQL query", function () {
+    const query = cfMigrations.getMigrationsSqlBundle({});
+    assert.equal(
+      query.query,
+      `BEGIN TRANSACTION;
+INSERT INTO migrations (revision, app_version, date_migrated) VALUES (?, ?, ?);
+VACUUM;
+COMMIT TRANSACTION;
+VACUUM;`
+    );
+    assert.equal(query.args.length, 4);
+  });
+});
+
+describe("Creating new table", function () {
+  it("should create a table", function () {
+    createBaseMigration();
+  });
+  it("should return correct SQL query", function () {
+    const query = cfMigrations.getMigrationsSqlBundle({});
+    assert.equal(
+      query.query,
+      `BEGIN TRANSACTION;
+INSERT INTO migrations (revision, app_version, date_migrated) VALUES (?, ?, ?);
+CREATE TABLE "species" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "name" TEXT NOT NULL, "origin" TEXT, "population" INTEGER);
+COMMIT TRANSACTION;
+VACUUM;`
+    );
+    assert.equal(query.args.length, 3);
+  });
+});
+
+describe("Creating new column", function () {
+  it("should create a table", function () {
+    createBaseMigration();
+  });
+  it("should add a column", function () {
+    cfMigrations.createMigration();
+    cfMigrations.addTableColumn("species", { language: { type: "text" } });
+  });
+  it("should return correct SQL query", function () {
+    const query = cfMigrations.getMigrationsSqlBundle({});
+    assert.equal(
+      query.query,
+      `BEGIN TRANSACTION;
+INSERT INTO migrations (revision, app_version, date_migrated) VALUES (?, ?, ?);
+CREATE TABLE "species" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "name" TEXT NOT NULL, "origin" TEXT, "population" INTEGER);
+INSERT INTO migrations (revision, app_version, date_migrated) VALUES (?, ?, ?);
+ALTER TABLE "species" ADD COLUMN "language" TEXT;
+COMMIT TRANSACTION;
+VACUUM;`
+    );
+    assert.equal(query.args.length, 6);
+  });
+});
